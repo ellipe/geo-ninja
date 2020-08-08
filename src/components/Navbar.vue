f6ee0ce0d819893d2e1ea257585190cc27a7205f
Luis Felipe Bertel <ellipe@gmail.com>
2020-08-08 11:48:32 -0500

    added routes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <router-link :to="{ name: 'Home' }">
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+    </router-link>
+
+    <v-spacer></v-spacer>
+    <router-link :to="{ name: 'About' }">
+      <v-btn text>
+        <span class="mr-2">About</span>
+      </v-btn>
+    </router-link>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
